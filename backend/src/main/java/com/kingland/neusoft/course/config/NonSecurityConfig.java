69ac8b9a71cab199ecda538849160e3d88d8f14f
gavwu <gavin.guohao.wu@kingland.com>
2022-05-12 15:39:31 +0800

    add note for gradle

--- backend/src/main/java/com/kingland/neusoft/course/config/NonSecurityConfig.java
+++ backend/src/main/java/com/kingland/neusoft/course/config/NonSecurityConfig.java
@@ -1,20 +1,11 @@
 package com.kingland.neusoft.course.config;
 
-import com.kingland.neusoft.course.mapper.UserMapper;
-import com.kingland.neusoft.course.security.ResponsiveAuthenticationFailureHandler;
-import com.kingland.neusoft.course.security.ResponsiveAuthenticationSuccessHandler;
-import com.kingland.neusoft.course.service.UserDetailServiceImpl;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * @author KSC
