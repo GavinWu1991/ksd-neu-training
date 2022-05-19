223b989f6468bff82ec29c8ae1b60e3f5999796c
gavwu <gavin.guohao.wu@kingland.com>
2022-05-19 14:02:15 +0800

    enable permission check annotation

--- backend/src/main/java/com/kingland/neusoft/course/config/SecurityConfig.java
+++ backend/src/main/java/com/kingland/neusoft/course/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -22,6 +23,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Profile("Security")
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserMapper userMapper;
